5a9256b2f872b2cb2a1d70479ec853bc9f8e39b5
Ravindra Singh <ravindra12329@gmail.com>
2024-04-23 07:31:07 +0530

    interface exmaples added

---
+++ src/com/rsg/interfaces/Child.java
@@ -0,0 +1,11 @@
+package com.rsg.interfaces;
+
+public class Child extends Parent {
+
+	public static void main(String[] args) {
+		Child child = new Child();
+
+		child.m1();
+	}
+
+}
---
+++ src/com/rsg/interfaces/Test.java
@@ -0,0 +1,5 @@
+package com.rsg.interfaces;
+
+public class Test implements Inter1, Interf2{
+
+}
