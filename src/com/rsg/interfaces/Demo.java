62ad7ef259dec2288df00b716cf382d6048e7554
Ravindra Singh <ravindra12329@gmail.com>
2024-04-29 07:16:27 +0530

    Demo file modified

--- src/com/rsg/interfaces/Demo.java
+++ src/com/rsg/interfaces/Demo.java
@@ -1,19 +1,31 @@
 package com.rsg.interfaces;
 
-public class Demo {
+public class Demo implements Right, Left{
 	
+	@Override
 	public void m1() {
 		
-		System.out.println("Hi good morning all.");
+		System.out.println("m1 from demo!!!");
+		
 	}
-
+	
+	
 	public static void main(String[] args) {
 		
 		Demo demo = new Demo();
 		
 		demo.m1();
-	
-
+		
+		Right right = new Demo();
+		
+		right.m1();
+		
+		Left left = new Demo();
+		
+		left.m1();
+		
 	}
+	
+	
 
 }
