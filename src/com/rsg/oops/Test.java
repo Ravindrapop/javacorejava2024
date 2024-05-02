e445915f84e9e807875c02289c7cc5bbe9317505
Ravindra Singh <ravindra12329@gmail.com>
2024-05-02 08:03:50 +0530

    encapsulation example added

---
+++ src/com/rsg/oops/Test.java
@@ -0,0 +1,20 @@
+package com.rsg.oops;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Student s1 = new Student();
+		
+		s1.setId(101);
+		
+		System.out.println(s1.getId());
+		
+		s1.setName("Alok");
+		
+		
+		System.out.println(s1.getName());
+		
+	}
+
+}
