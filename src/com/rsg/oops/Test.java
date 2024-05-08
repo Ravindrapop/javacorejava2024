db9d2651e497d5dc4cbf430a83e5237d7cabbe69
Ravindra Singh <ravindra12329@gmail.com>
2024-05-08 07:40:52 +0530

    test file modifies

--- src/com/rsg/oops/Test.java
+++ src/com/rsg/oops/Test.java
@@ -2,28 +2,13 @@ package com.rsg.oops;
 
 public class Test {
 	
+	
+	
 	public static void main(String[] args) {
 		
+		Student s1 = new Student();		
 		
-		Student s1 = new Student(101, "Ashish");
-		
-		s1.setId(111);
-		
-		System.out.println(s1.getId()+" "+s1.getName());
-		
-		Student s2 = new Student();
-		
-		s2.setId(102);
-		s2.setName("Bhavya");
-		
-		System.out.println(s2.getId()+" "+s2.getName());
-		
-		
-		Object obj = new Object();
-		
-		
-		System.out.println(10);
-		
+		s1.setId(101);
 		
 		
 	}
