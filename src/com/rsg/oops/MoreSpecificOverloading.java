c2047dbf444801ac4577c96c8589c0347ffcf24c
Ravindra Singh <ravindra12329@gmail.com>
2024-05-13 07:27:44 +0530

    more specific examples added

---
+++ src/com/rsg/oops/MoreSpecificOverloading.java
@@ -0,0 +1,16 @@
+package com.rsg.oops;
+
+public class MoreSpecificOverloading {
+	
+	public void m1(StringBuffer sb) {
+		System.out.println("StringBuffer version");
+	}
+	
+	
+	public void m1(String s) {
+		System.out.println("String version");
+		
+	}
+	
+
+}
